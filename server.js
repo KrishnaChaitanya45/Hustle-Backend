203f19c202c5a363af17f5f0f00239e25f7037d7
Krishna Chaitanya Kommera <kommerakrishnachaitanya@gmail.com>
2023-03-17 16:07:03 +0530

    Fixed multer issue - 8

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ const storage = multer.diskStorage({
 });
 app.use(multer({ storage: storage }).single("image"));
 app.use(express.json());
-app.use("/uploads", express.static(__dirname + "/tmp"));
-app.use("/uploads", express.static("tmp"));
+app.use(express.static(__dirname + "/tmp"));
+app.use("/tmp", express.static("tmp"));
 app.use("/api/v1/tasks/", authenticateJWT, MainTask);
 app.use("/api/v1/auth/", User);
 app.use("/api/v1/user", Habit);
