7b32a008cb9e78921348e22b53ab56c66d30026f
Krishna Chaitanya Kommera <kommerakrishnachaitanya@gmail.com>
2023-03-27 11:24:04 +0530

    added timestamps to the subtasks

--- models/GoalTracker/SubTask.js
+++ models/GoalTracker/SubTask.js
@@ -1,35 +1,41 @@
 const mongoose = require("mongoose");
-const taskSchema = new mongoose.Schema({
-  starttime: {
-    type: Date,
+const taskSchema = new mongoose.Schema(
+  {
+    starttime: {
+      type: Date,
+    },
+    title: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    completedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    duration: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    status: {
+      type: String,
+      enum: ["completed", "working", "pending", "timeexceeded"],
+      default: "pending",
+    },
+    belongsTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "MainTask",
+    },
+    deadline: {
+      type: Date,
+    },
   },
-  title: {
-    type: String,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  completedAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  description: {
-    type: String,
-  },
-  status: {
-    type: String,
-    enum: ["completed", "working", "pending", "timeexceeded"],
-    default: "pending",
-  },
-  belongsTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "MainTask",
-  },
-  deadline: {
-    type: Date,
-  },
-});
+  { timestamps: true }
+);
 
 const SubTask = mongoose.model("SubTask", taskSchema);
 SubTask.createCollection().then(function (collection) {});
